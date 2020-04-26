c05d6c19b7d9ca2a22bc2dd6daf69bc92f322188
tsarvasu <64128923+tsarvasu@users.noreply.github.com>
2020-04-26 22:17:06 +0530

    changes

--- submit.php
+++ submit.php
@@ -74,11 +74,11 @@
         <div class="row">
             <div class="col-md-12">
                 <div class="well well-sm">
-                    <h1 style="color:#3b7f25;text-align: center;">
+                    <h2 style="color:#3b7f25;text-align: center;">
                     <?php
                         if(isset($_GET['msg'])) echo $_GET['msg']
                         ?>
-                        </h1>
+                        </h2>
                 </div>
             </div>
         </div>
